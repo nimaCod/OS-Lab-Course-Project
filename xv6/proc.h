133d182238becf11a04cf181fe98005095d21ccb
AliGhAliGh <ali09374505823@gmail.com>
2023-12-31 18:28:45 +0330

    bug fixed

--- xv6/proc.h
+++ xv6/proc.h
@@ -18,8 +18,6 @@ void set_proc_sched(struct proc *np);
 extern struct cpu cpus[NCPU];
 extern int ncpu;
 
-uint shared_syscall_num;
-
 // PAGEBREAK: 17
 //  Saved registers for kernel context switches.
 //  Don't need to save all the segment registers (%cs, etc),
