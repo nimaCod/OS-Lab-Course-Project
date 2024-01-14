78ea14bbbd32548a6d3c1472f8bd49949cd7f037
nima8tajik <nimi.tajik@ut.ac.ir>
2024-01-14 12:13:01 -0800

    Done close shm

--- xv6/tms.c
+++ xv6/tms.c
@@ -12,42 +12,45 @@ struct shm_cnt
 
 int main(int argc, char *argv[])
 {
-    if (fork())
+    if (!fork())
     {
-        if (fork())
-        {
-            struct shm_cnt *counter;
-            open_sharedmem(1, (char **)&counter);
-            uacquire(&(counter->lock));
-            counter->cnt++;
-            urelease(&(counter->lock));
-            wait();
-            close_sharedmem(1);
-        }
-        else
-        {
-            struct shm_cnt *counter;
-            open_sharedmem(1, (char **)&counter);
-            uacquire(&(counter->lock));
-            counter->cnt++;
-            urelease(&(counter->lock));
-            close_sharedmem(1);
-            exit();
-            return 0;
-        }
-        wait();
-        exit();
-        return 0;
+        // if (!fork())
+        // {
+        //     struct shm_cnt *counter;
+        //     open_sharedmem(1, (char **)&counter);
+        //     uacquire(&(counter->lock));
+        //     counter->cnt++;
+        //     urelease(&(counter->lock));
+        //     wait();
+        //     close_sharedmem(1);
+        // }
+        // else
+        // {
+        //     struct shm_cnt *counter;
+        //     open_sharedmem(1, (char **)&counter);
+        //     uacquire(&(counter->lock));
+        //     counter->cnt++;
+        //     urelease(&(counter->lock));
+        //     close_sharedmem(1);
+        //     exit();
+        //     return 0;
+        // }
+        // wait();
+        // exit();
+        // return 0;
     }
     else
     {
         struct shm_cnt *counter;
         open_sharedmem(1, (char **)&counter);
-        uacquire(&(counter->lock));
-        counter->cnt++;
-        urelease(&(counter->lock));
+        // uacquire(&(counter->lock));
+        // counter->cnt++;
+        // urelease(&(counter->lock));
         close_sharedmem(1);
-        exit();
-        return 0;
+        wait();
+        // exit();
+        // return 0;
     }
+    exit();
+    return 0;
 }
--- xv6/vm.c
+++ xv6/vm.c
@@ -32,8 +32,7 @@ seginit(void)
 // Return the address of the PTE in page table pgdir
 // that corresponds to virtual address va.  If alloc!=0,
 // create any required page table pages.
-static pte_t *
-walkpgdir(pde_t *pgdir, const void *va, int alloc)
+pte_t * walkpgdir(pde_t *pgdir, const void *va, int alloc)
 {
   pde_t *pde;
   pte_t *pgtab;
